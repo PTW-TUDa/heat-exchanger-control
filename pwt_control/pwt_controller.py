2985e9addf76c2b44c864f56f3665d1a400c803a
JanWittTUD <jan-niklas.witt@stud.tu-darmstadt.de>
2025-09-23 16:25:40 +0200

    Implemented logic

--- pwt_control/pwt_controller.py
+++ pwt_control/pwt_controller.py
@@ -6,7 +6,7 @@ from numpy import asarray
 class PwtController(RuleBased):
     def control_rules(self, observation):
         if observation["pwt_system_state"] != 4:
-            action = [0]
+            action = [1,1,1,1,3,0]
         else:
-            action = [100]
+            action = [1,1,1,1,3,observation["machine_heating_consumption_target"]]
         return asarray(action)
