aec6c58758960a57a366475a44902217d8c8dd29
J.Witt <jan-niklas.witt@stud.tu-darmstadt.de>
2025-09-24 11:15:50 +0200

    Trying again

--- pwt_control/pwt_controller.py
+++ pwt_control/pwt_controller.py
@@ -6,7 +6,7 @@ from numpy import asarray
 class PwtController(RuleBased):
     def control_rules(self, observation):
         if observation["pwt_system_state"] != 4:
-            action = [1,1,1,1,3,0]
+            action = [0]
         else:
-            action = [1,1,1,1,3,observation["HKK_HWV.300.ThHy_P.WMZ300"]]
+            action = [10]
         return asarray(action)
