53c96f8af553871b719dc6274c1e301f14180808
J.Witt <jan-niklas.witt@stud.tu-darmstadt.de>
2025-09-24 09:31:29 +0200

    Test_run_1

--- pwt_control/pwt_controller.py
+++ pwt_control/pwt_controller.py
@@ -8,5 +8,5 @@ class PwtController(RuleBased):
         if observation["pwt_system_state"] != 4:
             action = [1,1,1,1,3,0]
         else:
-            action = [1,1,1,1,3,observation["machine_heating_consumption_target"]]
+            action = [1,1,1,1,3,observation["HKK_HWV.300.ThHy_P.WMZ300"]]
         return asarray(action)
