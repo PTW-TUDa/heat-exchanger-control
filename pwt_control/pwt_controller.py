157f92f7983ea2333fd88406e95e4ce0f50a179c
Andreas Clement <a.clement@ptw.tu-darmstadt.de>
2026-03-11 17:18:09 +0100

    setup for aae experiment

--- pwt_control/pwt_controller.py
+++ pwt_control/pwt_controller.py
@@ -1,12 +1,15 @@
-
 from eta_ctrl.agents.rule_based import RuleBased
 from numpy import asarray
 
 
 class PwtController(RuleBased):
     def control_rules(self, observation):
-        if observation["pwt_system_state"] != 4:
-            action = [0.0]
+        if observation["pwt_system_state"] != 4 or observation["hnht_algorithm_permission"] == 0:
+            action = [-1.0]
         else:
-            action = observation["lastverlauf"]
+            action = (
+                observation["hex1_thermal_load"]
+                + observation["static_heating_thermal_load"]
+                + observation["central_machine_heating_thermal_load"]
+            )
         return asarray(action)
