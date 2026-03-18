a2e539445199c34607774d3d6791a05902ae2bd2
J.Witt <jan-niklas.witt@stud.tu-darmstadt.de>
2026-03-18 16:38:26 +0100

    Added observations of the HNLT

--- pwt_control/main.py
+++ pwt_control/main.py
@@ -20,7 +20,7 @@ def experiment() -> None:
     get_logger(level=1, log_format="logname")
     root_path = pathlib.Path(__file__).parent
     experiment = EtaCtrl(root_path=root_path, config_relpath="", config_name="aae_experiment")
-    experiment.play(series_name="experiment_aae_3d", run_name="live_run_test")
+    experiment.play(series_name="experiment_aae_3d", run_name="live_run_observations")
 
 
 if __name__ == "__main__":
